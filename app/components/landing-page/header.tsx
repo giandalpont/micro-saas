b67800af03287aba3d8a08a012fc370ce4236aca
Gian Dal Pont <giandalpont@gmail.com>
2025-07-12 22:31:08 -0300

    Configurado AUth

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image"
 import { Button } from "../ui"
+import { auth } from "@/app/lib/auth"
+import { manageAuth } from "@/app/actions/manage-auth";
 
-export const Header = () => (
+export const Header = async () => {
+const session = await auth();
+  return (
     <div className="absolute top-0 left-0 right-0 max-w-7xl mx-auto flex items-center justify-between py-10">
       <div className="flex items-center gap-4">
         <Image width={27} height={32} src="/logo.svg" alt="ProjectInBio Logo" />
         <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
       </div>
       <div className="flex items-center gap-4">
-        <Button>Minha Página</Button>
-        <Button>Sair</Button>
+          {session && <Button>Minha Página</Button>}
+          <form action={manageAuth}>
+            <Button>{session ? "Sair" : "Login"}</Button>
+          </form>
       </div>
     </div>
   )
+}
 
 export default Header
